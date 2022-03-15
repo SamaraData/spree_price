d8e096d5647251359e88e7257d490d4dbf7a68e7
James Nickerson <jNicker@users.noreply.github.com>
2022-03-14 23:17:28 -0700

    Update store_decorator.rb

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,4 +1,4 @@
 Spree::Store.class_eval do
-  has_many :price_books, -> { order('spree_store_price_books.priority ASC') }, through: :store_price_books
   has_many :store_price_books
+  has_many :price_books, -> { order('spree_store_price_books.priority ASC') }, through: :store_price_books
 end
